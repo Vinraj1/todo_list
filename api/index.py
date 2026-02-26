298482458cc4f028e1f36ae04f4d01b3f244ae85
Vinraj1 <160110182+Vinraj1@users.noreply.github.com>
2026-02-26 13:30:50 +0000

    message

--- api/index.py
+++
@@ -1,7 +0,0 @@
-import os
-from django.core.wsgi import get_wsgi_application
-
-# Replace 'todo.settings' with your actual project settings path
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "todo.settings")
-
-app = get_wsgi_application()
