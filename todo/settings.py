39c817ed9e2a4044e90b464e12fde147f5f355d1
Vinraj1 <160110182+Vinraj1@users.noreply.github.com>
2026-02-27 01:27:54 +0000

    Update database settings to use PostgreSQL and add pymysql for MySQL compatibility

--- todo/settings.py
+++ todo/settings.py
@@ -78,14 +78,17 @@ WSGI_APPLICATION = 'todo.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 import os
-from pathlib import Path
 
-BASE_DIR = Path(__file__).resolve().parent.parent
+import os
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': '/tmp/db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'HOST': os.environ.get('DB_HOST'),
+        'PORT': '5432',
     }
 }
 
